14f258a37e4025818485c19df0e366464cc9e0c4
Rangga-PR <ranggaputraraharja@gmail.com>
2022-04-15 22:10:19 +0700

    add useError

---
+++ src/hooks/useError.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect, useCallback } from "react";
+
+const useError = (err: Error) => {
+  const [error, setError] = useState(err);
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  const dispatchError = useCallback((err) => {
+    setError(err);
+  }, []);
+
+  return dispatchError;
+};
