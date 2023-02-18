778af266fae9e3fcee8f37ac94c39c0d7f832b1d
Rangga-PR <ranggaputraraharja@gmail.com>
2023-02-18 11:32:00 +0700

    add useOnWindowScroll

---
+++ src/hooks/useOnWindowScroll.tsx
@@ -0,0 +1,16 @@
+import { useRef, useEffect } from "react";
+
+const useOnWindowScroll = (callback: () => void) => {
+  const listener = useRef(null);
+
+  useEffect(() => {
+    if (listener.current)
+      window.removeEventListener("scroll", listener.current);
+    listener.current = window.addEventListener("scroll", callback);
+    return () => {
+      window.removeEventListener("scroll", listener.current);
+    };
+  }, [callback]);
+};
+
+export default useOnWindowScroll;
