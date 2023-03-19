f352562bdb6d3be8898d885225cd8c8e2c34549b
Rangga-PR <ranggaputraraharja@gmail.com>
2023-03-20 00:18:56 +0700

    add useIsomorphicEffect

---
+++ src/hooks/useIsomorphicEffect.tsx
@@ -0,0 +1,6 @@
+import { useLayoutEffect, useEffect } from "react";
+
+const useIsomorphicEffect =
+  typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+export default useIsomorphicEffect;
