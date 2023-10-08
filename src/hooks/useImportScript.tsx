10030eb06c0d3f7aa513cc0f2534c54bec1dce5e
Rangga-PR <ranggaputraraharja@gmail.com>
2023-10-08 16:11:19 +0700

    refactor useImportScript hook

--- src/hooks/useImportScript.tsx
+++ src/hooks/useImportScript.tsx
@@ -1,24 +1,64 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-const UseImportScript = (sourceUrl: string, async: boolean = false) => {
+type Booleanish = boolean | "true" | "false";
+type HTMLAttributeReferrerPolicy =
+  | ""
+  | "no-referrer"
+  | "no-referrer-when-downgrade"
+  | "origin"
+  | "origin-when-cross-origin"
+  | "same-origin"
+  | "strict-origin"
+  | "strict-origin-when-cross-origin"
+  | "unsafe-url";
+
+interface ScriptHTMLAttributes {
+  async?: Booleanish;
+  charset?: string;
+  crossorigin?: string;
+  defer?: Booleanish;
+  integrity?: string;
+  nomodule?: Booleanish;
+  referrerpolicy?: HTMLAttributeReferrerPolicy;
+  nonce?: string;
+  src?: string;
+  type?: string;
+}
+
+interface Option {
+  location: "head" | "body";
+  attributes: ScriptHTMLAttributes;
+}
+
+const UseImportScript = (
+  sourceUrl: string,
+  option: Option = { location: "head", attributes: { async: true } }
+) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const scriptRef = useRef();
+  const { location, attributes } = option;
 
   useEffect(() => {
     const script = document.createElement("script");
     script.src = sourceUrl;
-    script.async = async;
-    document.body.appendChild(script);
+
+    for (const [key, value] of Object.entries(attributes)) {
+      script.setAttribute(key, value);
+    }
+
+    scriptRef.current = script;
+    document[location].appendChild(script);
 
     script.onload = () => {
       setIsLoaded(true);
     };
 
     return () => {
-      document.body.removeChild(script);
+      document[location].removeChild(script);
     };
   }, []);
 
-  return { isLoaded };
+  return { isLoaded, script: scriptRef };
 };
 
 export default UseImportScript;
